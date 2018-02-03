bb4aba8b87d521ec750b3fa275b54edbffca5b04
Ingrid Rosselis <irscunha@dcc.ufmg.br>
2018-02-03 12:27:02 +0000

    fixed minor issues

--- src/MyGR.java
+++ src/MyGR.java
@@ -7,6 +7,10 @@ import java.io.IOException;
 
 public class MyGR extends GRModel{
 
+    protected int maxDepth = 0;
+
+    protected int maxInitialDepth = 0;
+
     public MyGR(File file) throws IOException {
         setGrammar(new Grammar(file));
 
